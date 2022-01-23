9d5a72f731e5b07a5e94ab5ef0c4594010f364f6
Shoikot <96073081+shykat1@users.noreply.github.com>
2022-01-24 01:23:48 +0600

    Create String and char.kt

---
+++ String and char.kt
@@ -0,0 +1,18 @@
+fun main (args: Array<String>): Unit{
+
+    var name : String = "Kotlin"
+    //print(name:: class)
+    print(name.dropLast(2))
+
+    print("kotlin length is ${name.length}")
+
+    var sentence: String="""
+        Hey my name is sokal.
+        i can be a multi line sentence.
+        just use three qutoation and then boom.
+    """.trimIndent()
+
+    var name1: Char = 'k'// single quotes
+
+
+}
