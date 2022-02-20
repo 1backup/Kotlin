55f3d06e419e76910c2654415e8414faea24ff06
Shoikot <96073081+shykat1@users.noreply.github.com>
2022-02-21 01:05:58 +0600

    Create 1.19 backtick as escape.kt

---
+++ Basic/1.19 backtick as escape.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>)
+{
+ var l = `in`()
+
+}
+
+// how to use the kotlin keyword for your  own
+
+// in is key word in kotlin watch how we use it
+
+fun `in`() // to use you have to use the bactick
+{
+
+    print("fdsfsa")
+}
